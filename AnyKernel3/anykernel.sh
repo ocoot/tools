6561de470e3f56b419257df6b76f549065a45cdc
ocoot <cot48s@gmail.com>
2019-06-21 23:37:07 +0700

    Bring in my modified Compiler Script and AnyKernel Folders

---
+++ AnyKernel3/anykernel.sh
@@ -0,0 +1,50 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+do.devicecheck=1
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=mido
+device.name2=redmi note 4
+device.name3=Redmi Note 4
+device.name4=Redmi Note 4x
+supported.versions=8.1.0 - 9
+'; } # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+chmod -R 750 $ramdisk/*;
+chown -R root:root $ramdisk/*;
+
+
+## Treble Check
+is_treble=$(file_getprop /system/build.prop "ro.treble.enabled");
+if [ ! "$is_treble" -o "$is_treble" == "false" ]; then
+  ui_print " ";
+  ui_print "Only supports Treble ROMS!";
+  exit 1;
+fi;
+
+## AnyKernel install
+dump_boot;
+
+# begin ramdisk changes
+
+write_boot;
+## end install
+
---
+++ build.sh
@@ -0,0 +1,101 @@
+#Custom Build Script
+
+###############################################
+#                                             #
+# OCTOPUS PROJECT-X KERNEL REDMI NOTE 4X/MIDO #
+# COMPILING WITH GCC-10 AND CLANG-5484270     #
+#                                             #
+###############################################
+
+
+#
+# - 2019 - Modification by me "Octo O", to support my compiler device
+#
+# This software is licensed under the terms of the GNU General Public
+# License version 2, as published by the Free Software Foundation, and
+# may be copied, distributed, and modified under those terms.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# Please maintain this if you use this script or any part of it
+#
+
+
+# Color Code Script
+green='\033[01;32m'
+red='\033[01;31m'
+cyan='\033[01;36m'
+blue='\033[01;34m'
+blink_red='\033[05;31m'
+restore='\033[0m'
+nocol='\033[0m'
+
+
+# Init Script
+LC_ALL=C date +%Y-%m-%d
+date=`date +"%Y%m%d-%H%M"`
+BUILD_START=$(date +"%s")
+KERNEL_DIR=$PWD
+ZIP_FOLDER=build
+REPACK_DIR=$KERNEL_DIR/AnyKernel3
+OUT=$KERNEL_DIR/out
+VERSION=`date +"%Y%m%d"`
+KERNEL_CODE="DELTA"
+export ARCH=arm64 && export SUBARCH=arm64
+export CROSS_COMPILE="/home/octo/Kernel/aarch64-linux-gnu/bin/aarch64-linux-gnu-"
+
+
+# Tweakable Stuff
+export KBUILD_BUILD_USER="ExGurita"
+export KBUILD_BUILD_HOST="LineageOS"
+export CLANG_TCHAIN="/home/octo/Kernel/clang-5484270/bin/clang"
+export KBUILD_COMPILER_STRING="$(${CLANG_TCHAIN} --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')"
+
+
+#COMPILATION SCRIPTS
+echo -e "$green"
+echo "--------------------------------------------------------"
+echo "      Initializing build to compile Ver: $KERNEL_CODE    "
+echo "--------------------------------------------------------"
+
+
+echo -e "$blink_red***********************************************"
+echo "          Initialising DEFCONFIG        "
+echo -e "***********************************************$nocol"
+make O=out mido_defconfig
+
+
+echo -e "$blink_red***********************************************"
+echo "          Compiling Kernel        "
+echo -e "***********************************************$nocol"
+make -j$(nproc --all) O=out ARCH=arm64 \
+                      CC="/home/octo/Kernel/clang-5484270/bin/clang" \
+                      CLANG_TRIPLE="aarch64-linux-gnu-" \
+                      CROSS_COMPILE="/home/octo/Kernel/aarch64-linux-gnu/bin/aarch64-linux-gnu-"
+
+
+echo -e "$blink_red***********************************************"
+echo "          Copying zImage        "
+echo -e "***********************************************$nocol"
+cd $REPACK_DIR
+cp $KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb $REPACK_DIR/
+
+
+
+echo -e "$blink_red***********************************************"
+echo "          Making Flashable Zip        "
+echo -e "***********************************************$nocol"
+FINAL_ZIP="mido-octopusx-${VERSION}.zip"
+zip -r9 "${FINAL_ZIP}" *
+cp *.zip $OUT
+rm *.zip
+cd $KERNEL_DIR
+rm AnyKernel3/Image.gz-dtb
+mv $OUT/*.zip $ZIP_FOLDER/
+
+BUILD_END=$(date +"%s")
+DIFF=$(($BUILD_END - $BUILD_START))
+echo -e "$blink_red Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
